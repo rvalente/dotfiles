7c8c77f782bdb18af9260169a507ca5da43a3fef
Ron Valente <ronald.valente@gmail.com>
2018-09-27 06:16:53 -0400

    move install script to scripts dir, remove symlinking, relocate Brewfile to .Brewfile

---
+++ scripts/dotfiles.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+## Shell Opts ----------------------------------------------------------------
+# Exit on any non-zero exit code
+set -o errexit
+
+# Exit on any unset variable
+set -o nounset
+
+# Pipeline's return status is the value of the last (rightmost) command
+# to exit with a non-zero status, or zero if all commands exit successfully.
+set -o pipefail
+
+# Enable tracing
+set -o xtrace
+
+## Variables -----------------------------------------------------------------
+REWFILE="${DOTFILES}/.Brewfile"
+
+## Main ----------------------------------------------------------------------
+
+# Check for Homebrew and install if we don't have it
+if test ! "$(which brew)"; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Turn off Homebrew Analytics; Before we do anything else!
+brew analytics off
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+
+# Check if we have a custom brewfile
+echo "Checking for custom Brewfile at .Brewfile.local"
+if [ -f "${HOME}/.Brewfile.local" ]; then
+  BREWFILE="$HOME/.Brewfile.local"
+fi
+
+# Install User Packages
+echo "Install Packages from ${BREWFILE}..."
+brew bundle --file="$BREWFILE"
