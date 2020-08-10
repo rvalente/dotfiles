3dbf5e7e69cee7764ad2fd520984d7cf11f448d8
Ron Valente <ronald.valente@gmail.com>
2020-08-10 09:08:43 -0400

    added multimarkdown

--- scripts/install_ports.sh
+++ scripts/install_ports.sh
@@ -4,7 +4,10 @@
 sudo port selfupdate
 
 ## Declare our array of packages we need
-PORTS=(exa fzf git-delta git go zsh hugo shellcheck tree pv sqlite3 postgresql12 ripgrep watch jq nodejs14 xz pandoc fd fossil fping wget curl vault terraform-0.12 packer chruby ruby-install tmux tmux-pasteboard gnupg2 paperkey pinentry-mac zshdb zsh-completions zsh-autosuggestions zsh-syntax-highlighting starship python37 py37-pip py37-ansible port_cutleaves emacs-app)
+PORTS=(exa fzf git-delta git go zsh hugo shellcheck tree pv sqlite3 postgresql12 ripgrep watch jq nodejs14 xz pandoc fd
+fossil fping wget curl vault terraform-0.12 packer chruby ruby-install tmux tmux-pasteboard gnupg2 paperkey pinentry-mac
+zshdb zsh-completions zsh-autosuggestions zsh-syntax-highlighting starship python37 py37-pip py37-ansible port_cutleaves
+emacs-mac-app multimarkdown)
 
 ## Install our packages
 for port in $PORTS; do
