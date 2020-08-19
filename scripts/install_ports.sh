52c9c06fd530e1183522a8ccb9146aca6bd67a31
Ron Valente <ronald.valente@gmail.com>
2020-08-19 16:58:20 -0400

    upgrade terraform and use tectonic

--- scripts/install_ports.sh
+++ scripts/install_ports.sh
@@ -4,8 +4,8 @@
 sudo port selfupdate
 
 ## Declare our array of packages we need
-PORTS=(exa fzf git-delta git go zsh hugo shellcheck tree pv sqlite3 postgresql12 ripgrep watch jq nodejs14 xz pandoc fd
-fossil fping wget curl vault terraform-0.12 packer chruby ruby-install tmux tmux-pasteboard gnupg2 paperkey pinentry-mac
+PORTS=(tectonic exa fzf git-delta git go zsh hugo shellcheck tree pv sqlite3 postgresql12 ripgrep watch jq nodejs14 xz pandoc fd
+fossil fping wget curl vault terraform-0.13 packer chruby ruby-install tmux tmux-pasteboard gnupg2 paperkey pinentry-mac
 zshdb zsh-completions zsh-autosuggestions zsh-syntax-highlighting starship python37 py37-pip py37-ansible port_cutleaves
 emacs-app multimarkdown)
 
@@ -20,3 +20,4 @@ sudo port select --set python python37
 sudo port select --set python3 python37
 sudo port select --set pip pip37
 sudo port select --set pip3 pip37
+sudo port select --set terraform terraform0.13
